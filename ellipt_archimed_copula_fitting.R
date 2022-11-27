d6309edd038c2f76bcebe8069f79bcc34fe64ddc
EdytaPietrucha <edyta.pietrucha2@gmail.com>
2022-11-27 16:03:20 +0100

    Clean up ellipt_archimed_copula_fitting.R

--- ellipt_archimed_copula_fitting.R
+++ ellipt_archimed_copula_fitting.R
@@ -1,13 +1,12 @@
 # fitting elliptical i archimedean copulas
 # Transform the marginals to the unit interval
 pseudo_data <- data_log %>% pobs
-index_num <- pseudo_data %>% dim %>% tail(1)
 # The copulas described above
-copulas = list(normal = copula::normalCopula(dim = index_num, dispstr = "un"),
-               t = copula::tCopula(dim = index_num, dispstr = "un"),
-               frank = copula::frankCopula(dim = index_num),
-               clayton = copula::claytonCopula(dim = index_num),
-               gumbel = copula::gumbelCopula(dim = index_num))
+copulas = list(normal = copula::normalCopula(dim = indexes_num , dispstr = "un"),
+               t = copula::tCopula(dim = indexes_num , dispstr = "un"),
+               frank = copula::frankCopula(dim = indexes_num ),
+               clayton = copula::claytonCopula(dim = indexes_num ),
+               gumbel = copula::gumbelCopula(dim = indexes_num ))
 # 'mpl' - Maximum pseudo-likelihood estimator
 fits_ellip_arch = sapply(copulas,
                          function(x) copula::fitCopula(x, data = pseudo_data, method = "mpl"))
