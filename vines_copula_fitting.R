0990c70488f13ca7e4519e2c8378ecda4b480e19
EdytaPietrucha <edyta.pietrucha2@gmail.com>
2022-11-30 11:47:52 +0100

    Creating vines_copula_fitting.R file where vine copulas are calibrated

---
+++ vines_copula_fitting.R
@@ -0,0 +1,38 @@
+# fitting vines copulas
+set.seed(127)
+# C-vine structure - methodology is based on assumption that main root for each edge
+# corresponds to variable which has the highest value of sum from Spearman correlation matrix with all variables
+correlation_matrix <- spearman_log
+cvine_struc <- cvine_structure(correlation_matrix %>% colSums() %>% order())
+# C-vine structure - methodology is based on the assumption that companies from one sector are the most correlated
+# and should be inseparably next to each other - to pick up the best structure of indexes an investiagation has been conducted
+#  NOT RUN!
+# source('selecting_dvine_structure.R')
+dvine_final_struc <- read_csv('dvine_final_structure.csv') 
+dvine_final_struc <- dvine_final_struc %>% dplyr::select(x) %>% unlist() %>% as.numeric()
+dvine_struc <- dvine_structure(dvine_final_struc)
+# structure based  on Dissman's structure selection algorithm (https://cran.r-project.org/web/packages/rvinecopulib/rvinecopulib.pdf page 31)
+rvine_struc <- NA
+
+vine_structures <-  list('dvine' = dvine_struc,
+                         'cvine' = cvine_struc,
+                         'rvine' = rvine_struc
+)
+
+#plot(rvine_struc)
+fits_vines = lapply(vine_structures,
+                    function(x) vinecop(pseudo_data, structure = x, keep_data = TRUE, 
+                                        family_set = family, 
+                                        selcrit = 'loglik', tree_crit = 'rho', par_method = 'mle')
+)
+fits_vines %>% print()
+
+measures_vine_copulas <- list('AIC' = sapply(fits_vines, AIC),
+                              'BIC' = sapply(fits_vines, BIC),
+                              'Log-likelihood' = sapply(fits_vines, logLik)
+)
+measures_vine_copulas %>% print()
+# plot first two copula trees
+plot(fits_vines$dvine, edge_labels = "family_tau", tree = 1:2)
+plot(fits_vines$cvine, edge_labels = "family_tau", tree = 1:2)
+plot(fits_vines$rvine, edge_labels = "family_tau", tree = 1:2)
