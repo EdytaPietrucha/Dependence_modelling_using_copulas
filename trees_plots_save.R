910691022f3db60442be8b91d33c94d4cfd3a669
EdytaPietrucha <edyta.pietrucha2@gmail.com>
2023-09-26 14:54:14 +0200

    Plot trees from fitted vines

---
+++ trees_plots_save.R
@@ -0,0 +1,17 @@
+# saving generated figures in jpeg format in figures folder
+setwd(paste0(main_path,'/figures/vine_trees/'))
+
+jpeg(filename = "trees_dvine.jpeg",
+     width = 1200, height = 700)
+plot(vines$dvine, edge_labels = "family_tau", tree = 1:2)
+dev.off()
+
+jpeg(filename = "trees_cvine.jpeg",
+     width = 1200, height = 700)
+plot(vines$cvine, edge_labels = "family_tau", tree = 1:2)
+dev.off()
+
+jpeg(filename = "trees_rvine.jpeg",
+     width = 1200, height = 700)
+plot(vines$rvine, edge_labels = "family_tau", tree = 1:2)
+dev.off()
